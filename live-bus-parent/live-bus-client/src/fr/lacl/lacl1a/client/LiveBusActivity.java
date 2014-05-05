e460651c71ba74980ea234284b3c7b27900be172
EricONDOA <eric.ondoa@gmail.com>
2014-05-05 09:15:50 +0200

    with the html5 api webview

---
+++ live-bus-parent/live-bus-client/src/fr/lacl/lacl1a/client/LiveBusActivity.java
@@ -0,0 +1,35 @@
+package fr.lacl.lacl1a.client;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+/**
+ * LiveBusActivity test that html5 work with android app, we use the webview api
+ * 
+ *
+ */
+public class LiveBusActivity extends Activity {
+	private Button button;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		final Context context = this;
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Button button = (Button) findViewById(R.id.buttonUrl);
+		button.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View arg0) {
+				Intent intent = new Intent(context, WebViewActivity.class);
+				startActivity(intent);
+			}
+
+		});
+	}
+}
